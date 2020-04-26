2fc250901d2096fe0ca1db534641d6caf33fbffa
yaiza <yaiza@yaiza-VirtualBox>
2020-04-26 17:47:51 +0200

    Versio 0.2 del projecte prjava21

--- src/prjava21/prjava21.java
+++ src/prjava21/prjava21.java
@@ -26,6 +26,7 @@ public class prjava21 {
             bw.write("    </title>");bw.newLine();
             bw.write("  </head>");bw.newLine();
             bw.write("  <body>");bw.newLine();
+            bw.write("<h1>DAW2 M08uf4pr2</h1>");bw.newLine();
             bw.write("    Nova p&agrave;gina web");bw.newLine();
             bw.write("  </body>");bw.newLine();
             bw.write("</html>");bw.newLine();
